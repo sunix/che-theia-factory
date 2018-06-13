1d6c0a5b45da0e6d8c8e1eeeb30e9e2a180aa9b7
Sun Seng David TAN <sutan@redhat.com>
2018-06-13 11:48:14 +0000

    refactoring
    Signed-off-by: Sun Seng David TAN <sutan@redhat.com>

--- che-theia-factory-frontend-extension/src/browser/factory-theia-client.ts
+++ che-theia-factory-frontend-extension/src/browser/factory-theia-client.ts
@@ -12,7 +12,7 @@
 import { injectable, inject } from "inversify";
 import { MessageService } from "@theia/core/lib/common";
 import { FrontendApplicationContribution, FrontendApplication } from '@theia/core/lib/browser';
-import { EnvVariablesServer } from "@theia/core/lib/common/env-variables";
+import { EnvVariablesServer, EnvVariable } from "@theia/core/lib/common/env-variables";
 import { Resources } from "./resources";
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import { Git, Repository } from "@theia/git/lib/common";
@@ -32,62 +32,63 @@ export class FactoryTheiaClient implements FrontendApplicationContribution {
         @inject(Git) protected readonly git: Git,
     ) { }
 
-    onStart(app: FrontendApplication): void {
+    async onStart(app: FrontendApplication) {
         const factoryid = queryString.parse(window.location.search)['factory-id'];
-
         if (!factoryid) {
             return;
         }
 
-        this.envVariablesServer.getVariables().then((envVariables) => {
+        var envVariables = await this.envVariablesServer.getVariables();
+
+        const cheApiExternalVar:  = this.getEnvVariable(envVariables, 'CHE_API_EXTERNAL');
+        const projectsRootEnvVar: EnvVariable = this.getEnvVariable(envVariables,'CHE_PROJECTS_ROOT');
+        if (projectsRootEnvVar && projectsRootEnvVar.value) {
+            projectsRoot = projectsRootEnvVar.value;
+        }
+        if (!cheApiExternalVar) {
+            return;
+        }
+        var projectsRoot = "/projects";
 
 
-            const cheApiExternalVar = envVariables.find(function(envVariable) { return envVariable.name === 'CHE_API_EXTERNAL' });
-            const projectsRootEnvVar = envVariables.find(function(envVariable) { return envVariable.name === 'CHE_PROJECTS_ROOT' });
+        var factory = new Resources(FactoryTheiaClient.axiosInstance, String(cheApiExternalVar.value));
+        var response: AxiosResponse<IFactory> = await factory.getById<IFactory>(factoryid);
 
-
-            if (!cheApiExternalVar) {
+        response.data.workspace.projects.forEach((project: IProjectConfig) => {
+            if (!project.source) {
                 return;
             }
 
-            var projectsRoot = "/projects";
-            if (projectsRootEnvVar && projectsRootEnvVar.value) {
-                projectsRoot = projectsRootEnvVar.value;
-            }
+            const source = project.source;
+            const projectPath = projectsRoot + project.path;
 
-            const factory = new Resources(FactoryTheiaClient.axiosInstance, String(cheApiExternalVar.value));
-            factory.getById<IFactory>(factoryid).then((response: AxiosResponse<IFactory>) => {
-                response.data.workspace.projects.forEach((project: IProjectConfig) => {
-                    if (!project.source) {
-                        return;
+            this.messageService.info(`Cloning ... ${source.location} to ${projectPath}...`)
+
+            //TODO try without sleep
+            setTimeout(() => {
+                this.git.clone(
+                    source.location,
+                    {
+                        localUri: projectPath
                     }
-
-                    const source = project.source;
-                    const projectPath = projectsRoot + project.path;
-
-                    this.messageService.info(`Cloning ... ${source.location} to ${projectPath}...`)
-                    setTimeout(() => {
-                        this.git.clone(
-                            source.location,
-                            {
-                                localUri: projectPath
-                            }
-                        ).then(
-                            (repo: Repository) => {
-                                this.messageService.info(`Project ${projectPath} successfully cloned.`);
-                                if (source.parameters['branch']) {
-                                    const options: Git.Options.Checkout.Branch = { branch: source.parameters['branch'] };
-                                    this.git.checkout(repo, options);
-                                }
-                            }
-                        ).catch((error) => {
-                            console.error(`Couldn't clone ${source.location} to ${projectPath}... ${error}`);
-                            this.messageService.error(`Couldn't clone ${source.location} to ${projectPath}... ${error}`);
-                        });
-                    }, 10000);
-
+                ).then(
+                    (repo: Repository) => {
+                        this.messageService.info(`Project ${projectPath} successfully cloned.`);
+                        if (source.parameters['branch']) {
+                            const options: Git.Options.Checkout.Branch = { branch: source.parameters['branch'] };
+                            this.git.checkout(repo, options);
+                        }
+                    }
+                ).catch((error) => {
+                    console.error(`Couldn't clone ${source.location} to ${projectPath}... ${error}`);
+                    this.messageService.error(`Couldn't clone ${source.location} to ${projectPath}... ${error}`);
                 });
-            });
-        })
+            }, 10000);
+
+        });
+    }
+
+    getEnvVariable(envVariables: EnvVariable[], name: string){
+        envVariables.find(function(envVariable) { return envVariable.name === name });
     }
 }
